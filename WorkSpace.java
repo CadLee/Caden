eee5a99c1f61e83e7cd65e35a80630352301492e
CadLee <77828405+CadLee@users.noreply.github.com>
2022-03-11 09:26:30 +0530

    Working adder with refreshing jframe

--- WorkSpace.java
+++ WorkSpace.java
@@ -1,6 +1,9 @@
 package IA.src;
 
+import com.sun.source.doctree.TextTree;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,10 +17,21 @@ public class WorkSpace implements ActionListener{
     private final JButton sCls = new JButton("Exit");
 
     private final JButton fAdd = new JButton("Add");
+    private final JButton fAddCfn = new JButton("Confirm");
+    private final JButton fAddCan = new JButton("Cancel");
+
     private final JButton fRem = new JButton("Remove");
+    private final JButton fRemCfn = new JButton("Confirm");
+    private final JButton fRemCan = new JButton("Cancel");
+
 
     private final JButton eAdd = new JButton("Add");
+    private final JButton eAddCfn = new JButton("Confirm");
+    private final JButton eAddCan = new JButton("Cancel");
+
     private final JButton eRem = new JButton("Remove");
+    private final JButton eRemCfn = new JButton("Confirm");
+    private final JButton eRemCan = new JButton("Cancel");
 
 
     private final JTextField hText;
@@ -29,16 +43,24 @@ public class WorkSpace implements ActionListener{
     private final JFrame frame = new JFrame();
     private final JFrame sign = new JFrame();
 
-    private static final String[] fArr = {"Steak Dinner","Cereal"};
-    private static final String[] eArr = {"Running 1hr","Workout Set 1"};
-    private static final int[] fCal = {1280, 440};
-    private static final int[] eCal = {760, 900};
-    private static final int finCal=0;
+    private JFrame fAdder = new JFrame();
+    private final JFrame fRemove = new JFrame();
 
-    public static JList<String> fList;
-    public static JList<String> eList;
+    private final JFrame eAdder = new JFrame();
+    private final JFrame eRemove = new JFrame();
 
-    private static int i;
+    private final TextField foodText = new TextField();
+    private final TextField fCalText = new TextField();
+
+
+
+    JList<food> fList = new JList<>();
+    DefaultListModel<food> fListM = new DefaultListModel<>();
+
+    JList<exer> eList = new JList<>();
+    DefaultListModel <exer> eListM = new DefaultListModel<>();
+
+
 
 
 
@@ -55,6 +77,8 @@ public class WorkSpace implements ActionListener{
         panel.setLayout(null);
 
         GUI ob_gui = new GUI();
+
+
 
         JLabel username = new JLabel("User : "+ob_gui.userOut());
         username.setBounds(10,10,80,10);
@@ -88,8 +112,7 @@ public class WorkSpace implements ActionListener{
         bmiCalc.addActionListener(this);
         panel.add(bmiCalc);
 
-
-        fList=new JList<>(fArr);
+        fList.setModel(fListM);
         fList.setVisibleRowCount(9);
         fList.setBounds(250,70,200,150);
         panel.add(fList);
@@ -99,11 +122,13 @@ public class WorkSpace implements ActionListener{
         panel.add(fScroll);
 
         fAdd.setBounds(250,230,100,30);
+        fAdd.addActionListener(this);
         panel.add(fAdd);
         fRem.setBounds(350, 230,100,30);
+        fRem.addActionListener(this);
         panel.add(fRem);
 
-        eList=new JList<>(eArr);
+        eList.setModel(eListM);
         eList.setVisibleRowCount(9);
         eList.setBounds(500,70,200,150);
         panel.add(eList);
@@ -124,6 +149,48 @@ public class WorkSpace implements ActionListener{
         sOut.addActionListener(this);
         panel.add(sOut);
 
+
+        // Food Adding components -------------------------------------
+
+
+        fAdder = new JFrame();
+        JPanel fAddPan = new JPanel();
+        fAdder.add(fAddPan);
+        fAdder.setLocation(400,300);
+        fAdder.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        fAdder.setSize(400,400);
+        fAddPan.setLayout(null);
+
+        JLabel fMsg = new JLabel("Please add food item and calories gained from the food.");
+        fMsg.setBounds(10,10,390,40);
+        fAddPan.add(fMsg);
+        JLabel foodLabel = new JLabel("Name of food :");
+        foodLabel.setBounds(10,60,100,30);
+        fAddPan.add(foodLabel);
+
+        foodText.setText("");
+        foodText.setBounds(120,60,200,30);
+        fAddPan.add(foodText);
+
+
+        JLabel fCalLabel = new JLabel("Calories :");
+        fCalLabel.setBounds(10,100,100,30);
+        fAddPan.add(fCalLabel);
+
+        fCalText.setText("");
+        fCalText.setBounds(120,100,200,30);
+        fAddPan.add(fCalText);
+
+        fAddCfn.setBounds(230,150,100,30);
+        fAddCfn.addActionListener(this);
+        fAddPan.add(fAddCfn);
+
+        fAddCan.setBounds(115,150,100,30);
+        fAddCan.addActionListener(this);
+        fAddPan.add(fAddCan);
+
+        fAdder.setVisible(false);
+
         frame.setVisible(true);
     }
 
@@ -141,20 +208,26 @@ public class WorkSpace implements ActionListener{
                 sLabel.setText("Invalid");
             }else if(b<=18.5){
                 sLabel.setText("Underweight");
+                //sLabel.setText("Thin af");
             }else if(b<25){
                 sLabel.setText("Normal");
+                //sLabel.setText("Average af");
             }else if(b<30){
                 sLabel.setText("Overweight");
+                //sLabel.setText("thicc af");
             }else if(b<35){
                 sLabel.setText("Obese");
+                //sLabel.setText("Phatt af");
             }else if(b>35){
                 sLabel.setText("Extremely Obese");
+                //sLabel.setText("Literally ur mom");
+
             }
         }
         else if(e.getSource() == sOut){
             sign.setSize(350,150);
             sign.setLocation(100,150);
-            sign.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            sign.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
             JPanel sPan=new JPanel();
             sign.add(sPan);
@@ -197,20 +270,110 @@ public class WorkSpace implements ActionListener{
             GUI.failure.setText("Logged out");
             GUI.pwdText.setText(null);
         }
-        else if (e.getSource() == fAdd){
-
-        }
-        else if (e.getSource() == fAdd){
-
-        }
-        else if (e.getSource() == fAdd){
-
-        }
-        else if (e.getSource() == fAdd){
-
-        }
-
-
         else if (e.getSource() == sCls) System.exit(0);
+
+
+
+        else if (e.getSource() == fAdd){
+            fAdder.setVisible(true);
+            frame.setEnabled(false);
+        }
+        else if (e.getSource() == fAddCfn){
+
+            System.out.println(foodText.getText());
+            System.out.println(Double.parseDouble(fCalText.getText()));
+
+            fListM.addElement(new food(foodText.getText(),Double.parseDouble(fCalText.getText())));
+
+            fList.updateUI();
+            frame.setEnabled(true);
+            fAdder.setVisible(false);
+            foodText.setText("");
+            fCalText.setText("");
+        }
+        else if (e.getSource() == fAddCan){
+            foodText.setText("");
+            fCalText.setText("");
+
+
+
+            frame.setEnabled(true);
+            fAdder.setVisible(false);
+        }
+        else if (e.getSource() == fRem){
+
+
+        }
+        else if (e.getSource() == eAdd){
+
+        }
+        else if (e.getSource() == eRem){
+            frame.setEnabled(true);
+            eAdder.dispose();
+        }
+
+
+    }
+
+    private class food {
+        String name;
+        double cal;
+
+        public food(String name, double cal) {
+            this.name = name;
+            this.cal = cal;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getCal() {
+            return cal;
+        }
+
+        public void setCal(double cal) {
+            this.cal = cal;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    private class exer {
+        String name;
+        double cal;
+
+        public exer(String name, double cal) {
+            this.name = name;
+            this.cal = cal;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getCal() {
+            return cal;
+        }
+
+        public void setCal(double cal) {
+            this.cal = cal;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
